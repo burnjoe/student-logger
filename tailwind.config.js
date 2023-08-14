3deb9464b5cc3da4cbb8e1d5d085675354e0fd9c
burnjoe <sabanajholo@gmail.com>
2023-08-15 07:38:08 +0800

    added dark green color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
       colors: {
         green: 'rgb(25,134,85)',
         lightGreen: 'rgb(213,237,219)',
+        darkGreen: 'rgb(23,79,46)',
         orange: 'rgb(250,99,65)',
         red: 'rgb(244,55,92)',
         gray: 'rgb(198,199,198)',

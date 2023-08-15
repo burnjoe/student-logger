fb6e4423f4c72050199124c2944172b1cdf7a4c3
burnjoe <sabanajholo@gmail.com>
2023-08-15 17:43:14 +0800

    fixed y overflow bug

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,15 +8,15 @@
 </head>
 <body>
 
-  <div class="h-screen overflow-x-hidden overflow-y-auto bg-lightGray flex flex-col justify-center items-center">
-    <div class="mx-8">
+  <div class="flex items-center justify-center min-h-screen bg-lightGray">
+    <div class="flex flex-col overflow-y-auto px-8 py-5 mb-5">
       <div class="max-w-sm">
         <div class="px-6">
           <img class="max-w-full" src="{{asset('img/pnc_header.png')}}" alt="">
         </div>
       </div>
   
-      <x-card class="mt-8 mb-10">
+      <x-card class="mt-8">
         <x-card.card-section class="flex flex-col items-center">
           <div class="px-12 pt-5">
             <img class="max-w-full" src="{{asset('img/ist_logo.png')}}" alt="">
@@ -31,31 +31,34 @@
             </x-card.card-section>
           </x-card>
 
-          <x-input-group>
-            <x-input type="text" placeholder="Username">
-              <x-slot name="leftIcon">
-                <i class="fa-solid fa-user"></i>
-              </x-slot>
-            </x-input>
-          </x-input-group>
-
-          <x-input-group>
-            <x-input type="password" placeholder="Password">
-              <x-slot name="leftIcon">
-                <i class="fa-solid fa-lock"></i>
-              </x-slot>
-            </x-input>
-          </x-input-group>
-
-          <span class="flex justify-end font-bold text-green text-sm transition-all hover:text-darkGreen">
-            <a href="#">Forgot Password?</a>
-          </span>
-
-          <div class="flex justify-center pb-5">
-            <x-button>
-              LOGIN
-            </x-button>
-          </div>
+          <form action="#" method="" class="flex flex-col space-y-4">
+            @csrf
+            <x-input-group>
+              <x-input type="text" placeholder="Username">
+                <x-slot name="leftIcon">
+                  <i class="fa-solid fa-user"></i>
+                </x-slot>
+              </x-input>
+            </x-input-group>
+  
+            <x-input-group>
+              <x-input type="password" placeholder="Password">
+                <x-slot name="leftIcon">
+                  <i class="fa-solid fa-lock"></i>
+                </x-slot>
+              </x-input>
+            </x-input-group>
+  
+            <span class="flex justify-end font-bold text-green text-sm transition-all hover:text-darkGreen">
+              <a href="#">Forgot Password?</a>
+            </span>
+  
+            <div class="flex justify-center pb-5">
+              <x-button>
+                LOGIN
+              </x-button>
+            </div>
+          </form>
         </x-card.card-section>
       </x-card>
     </div>

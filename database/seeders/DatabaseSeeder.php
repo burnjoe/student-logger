10d950da27552b28bc8941405a6d47532febc3d3
burnjoe <sabanajholo@gmail.com>
2023-08-17 10:42:27 +0800

    added model + factory-migration-seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,42 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Roles
+        $parent = Role::factory()->create([
+            'name' => 'Parent'
+        ]);
+
+        $admin = Role::factory()->create([
+            'name' => 'Admin'
+        ]);
+
+        $guard = Role::factory()->create([
+            'name' => 'Guard'
+        ]);
+
+        $librarian = Role::factory()->create([
+            'name' => 'Librarian'
+        ]);
+
+        $nurse = Role::factory()->create([
+            'name' => 'Nurse'
+        ]);
+
+        // Users
+        User::factory()->create([
+            'name' => 'Joy Sabana',
+            'email' => 'joysabana@gmail.com',
+            'role_id' => $parent->id,
+            'password' => bcrypt('burnjoe')
+        ]);
+
+        User::factory()->create([
+            'name' => 'Jholo Sabana',
+            'email' => 'sabanajholo@gmail.com',
+            'role_id' => $admin->id,
+            'password' => bcrypt('burnjoe')
+        ]);
+
     }
 }

bc94b8925f25893aefc44a266bc6ac899167ea38
burnjoe <sabanajholo@gmail.com>
2023-08-17 10:38:25 +0800

    reroute root to parentguardian/login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() { return view('login'); });  
+Route::get('/', function() { return redirect()->route('parent_guardian.login'); })->name('root');
 
-Route::get('/dashboard', function() { return view('dashboard'); });  
+Route::get('/university/login', function() { return view('login'); })->name('university.login');
+
+Route::get('/parentguardian/login', function() { return view('login'); })->name('parent_guardian.login');
+
+Route::get('/dashboard', function() { return view('dashboard'); })->name('dashboard');  

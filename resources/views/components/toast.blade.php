fd583c8b10609cdff6a78295aeae6e054f41e771
burnjoe <sabanajholo@gmail.com>
2023-08-19 11:25:56 +0800

    updated toast component

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,10 +1,27 @@
 @props([
     'shadow' => 'shadow-none',
     'bgColor' => 'bg-blue',
+    'align' => 'bottom-8 right-8'
 ])
 
-<x-card {{ $attributes->merge(['class' => 'absolute top-8 right-8']) }} shadow="{{ $shadow }}">
-  <x-card.card-section  bgColor="{{ $bgColor }}">
-    <span class="text-xs text-white"> {{ $slot }} </span>
-  </x-card.card-section>
+@php 
+  switch ($align) {
+    case 'top-left':
+      $alignmentClasses = "top-8 left-8";
+      break;
+    case 'top-right':
+      $alignmentClasses = "top-8 right-8";
+      break;
+    case 'bottom-left':
+      $alignmentClasses = "bottom-8 left-8";
+      break;
+    case 'bottom-right':
+    default:
+      $alignmentClasses = "bottom-8 right-8";
+      break;
+  }
+@endphp
+
+<x-card {{ $attributes->merge(['class' => 'absolute px-6 py-4 ' .$alignmentClasses ]) }} bgColor="{{ $bgColor }}" shadow="{{ $shadow }}">
+  {{ $slot }}
 </x-card>

ffe7861a81ef29e5eb1185845cac44c1717fdfd4
burnjoe <sabanajholo@gmail.com>
2023-08-22 15:46:09 +0800

    cleared User controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,34 +6,5 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    // Show Login Form
-    public function login() {
-        return view('auth.login');
-    }
-
-    // Authenticate User
-    public function authenticate(Request $request) {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => 'required'
-        ]);
-
-        if(auth()->attempt($credentials)) {
-            $request->session()->regenerate();
-
-            return redirect()->route('root');
-        }
-
-        return back()->withErrors(['email' => 'Invalid Login Credentials'])->onlyInput('email');
-    }
-
-    // Log User Out
-    public function logout(Request $request) {
-        auth()->logout();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        return redirect()->route('root');
-    }
+    // 
 }

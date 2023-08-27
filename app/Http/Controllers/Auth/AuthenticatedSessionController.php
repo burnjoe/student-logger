2c80f1b6049436248ebe5b3c66d6feac3bba04f6
burnjoe <sabanajholo@gmail.com>
2023-08-28 03:19:39 +0800

    seperated parent-guardian and university login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -15,19 +15,34 @@ class AuthenticatedSessionController extends Controller
     /**
      * Display the login view.
      */
-    public function create(): View
+    public function create($portal): View
     {
-        return view('auth.login');
+        if($portal === 'parent-guardian') {
+            return view('auth.login')->with([
+                'portal' => 'parent-guardian',
+                'title' => 'PARENT & GUARDIAN LOGIN'
+            ]);
+        } else if($portal === 'university') {
+            // FIND A WAY TO ADD MIDDLEWARE FOR IPFILTER
+            return view('auth.login')->with([
+                'portal' => 'university',
+                'title' => 'UNIVERSITY LOGIN'
+            ]);
+        }
+
+        return abort(404);
     }
 
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request, $portal): RedirectResponse
     {
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        session(['portal' => $portal]);
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
@@ -37,12 +52,14 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+        $portal = session('portal');
+
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect()->route('login', ['portal' => $portal]);
     }
 }

8ba2bba5c6589157b969d528e01469d44ce7b06d
burnjoe <sabanajholo@gmail.com>
2023-09-02 15:53:19 +0800

    added seeder for auth

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Roles
+        Role::create(['name' => 'admin'])
+            ->syncPermissions([
+                'view accounts'
+            ]);
+            
+        Role::create(['name' => 'guard']);
+        Role::create(['name' => 'librarian']);
+        Role::create(['name' => 'nurse']);
+        Role::create(['name' => 'parent']);
+        Role::create(['name' => 'guardian']);
+    }
+}

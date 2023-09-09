0e1d8e0523416b9dfc48b23ec5e8c75a589987a2
burnjoe <sabanajholo@gmail.com>
2023-09-09 16:20:44 +0800

    added scopeSearch fun

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -10,4 +10,8 @@ class Student extends Model
     use HasFactory;
 
     protected $fillable = ['student_no', 'last_name', 'first_name', 'middle_name', 'sex', 'civil_status', 'nationality', 'birthdate', 'birthplace', 'address', 'phone', 'email', 'account_type', 'program_id'];
+
+    public function scopeSearch($query, $value) {
+        $query->where('student_no', 'like', "%{$value}%");
+    }
 }

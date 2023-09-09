c4b014638d356f53cd0f25f6520feed8545deb12
burnjoe <sabanajholo@gmail.com>
2023-09-09 18:29:21 +0800

    updated debounce time

--- resources/views/livewire/includes/search.blade.php
+++ resources/views/livewire/includes/search.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-input-text wire:model.live.debounce.500ms="search" type="text" alignIcon="left" placeholder="Search">
+    <x-input-text wire:model.live.debounce.300ms="search" type="text" alignIcon="left" placeholder="Search">
         <i class="fa-solid fa-magnifying-glass"></i>
     </x-input-text>
 </div>

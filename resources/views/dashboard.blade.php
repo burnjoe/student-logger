cffc187686208997c740b74fda13d5a878dfa823
burnjoe <sabanajholo@gmail.com>
2023-09-15 16:05:18 +0800

    utilized profileable of user model

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout>
-    <div class="bg-white overflow-hidden shadow-sm rounded-lg">
-        <div class="p-6">
-            {{ 'Welcome, ' .Auth::user()->name. '!'}}
+    <x-card>
+        {{ 'Welcome, ' .Auth::user()->profileable->first_name. ' ' .Auth::user()->profileable->last_name. '!'}}
             {{-- <x-modal title="Modal 2"></x-modal>
             <button x-data x-on:click="$dispatch('open-modal')" class="px-3 py-1 bg-green text-white rounded-md">Button 2</button> --}}
-        </div>
-    </div>
+    </x-card>
 </x-app-layout>

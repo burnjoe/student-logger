700950546d2f2f1e8a2cca8240bd68cc9c462d40
burnjoe <sabanajholo@gmail.com>
2023-09-20 21:13:40 +0800

    added student create + view + edit

---
+++ resources/views/livewire/includes/students-form.blade.php
@@ -0,0 +1,187 @@
+<div>
+    <form wire:submit.prevent="update">
+        {{-- Student Number --}}
+        <div>
+            <x-input-label for="student_no" :value="__('Student Number')" :required="true" />
+            <x-input-text 
+                id="student_no"
+                wire:model="student_no"
+                name="student_no"
+                type="text"
+                placeholder="{{ __('Student Number') }}"
+                class="mt-1 bg-lightGray" />
+            <x-input-error class="mt-2" :messages="$errors->get('student_no')" />
+        </div>
+        {{-- Last Name --}}
+        <div class="mt-4">
+            <x-input-label for="last_name" :value="__('Last Name')" :required="true" />
+            <x-input-text 
+                id="last_name"
+                wire:model="last_name"
+                name="last_name"
+                type="text"
+                placeholder="{{ __('Last Name') }}"
+                class="mt-1 bg-lightGray" />
+            <x-input-error class="mt-2" :messages="$errors->get('last_name')" />
+        </div>
+        {{-- First Name --}}
+        <div class="mt-4">
+            <x-input-label for="first_name" :value="__('First Name')" :required="true" />
+            <x-input-text 
+                id="first_name"
+                wire:model="first_name"
+                name="first_name"
+                type="text"
+                placeholder="{{ __('First Name') }}"
+                class="mt-1 bg-lightGray" />
+            <x-input-error class="mt-2" :messages="$errors->get('first_name')" />
+        </div>
+        {{-- Middle Name --}}
+        <div class="mt-4">
+            <x-input-label for="middle_name" :value="__('Middle Name')" />
+            <x-input-text 
+                id="middle_name"
+                wire:model="middle_name"
+                name="middle_name"
+                type="text"
+                placeholder="{{ __('Middle Name') }}"
+                class="mt-1 bg-lightGray" />
+            <x-input-error class="mt-2" :messages="$errors->get('middle_name')" />
+        </div>
+        {{-- Sex --}}
+        <div class="mt-4 text-gray">
+            <x-input-label for="sex" :value="__('Sex')" :required="true" />
+            <x-input-select 
+                id="sex" 
+                wire:model="sex" 
+                name="sex" 
+                class="mt-1 bg-lightGray">
+                <option selected hidden>Sex</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+            </x-input-select>
+            <x-input-error class="mt-2" :messages="$errors->get('sex')" />
+        </div>
+        {{-- Civil Status --}}
+        <div class="mt-4 text-gray">
+            <x-input-label for="civil_status" :value="__('Civil Status')" :required="true" />
+            <x-input-select 
+                id="civil_status" 
+                wire:model="civil_status" 
+                name="civil_status" 
+                class="mt-1 bg-lightGray">
+                <option selected hidden>Civil Status</option>
+                <option value="Single">Single</	option>
+                <option value="Married">Married</option>
+                <option value="Divorced">Divorced</option>
+                <option value="Widowed">Widowed</option>
+            </x-input-select>
+            <x-input-error class="mt-2" :messages="$errors->get('civil_status')" />
+        </div>
+        {{-- Nationality --}}
+        <div class="mt-4">
+            <x-input-label for="nationality" :value="__('Nationality')" :required="true"/>
+            <x-input-text 
+                id="nationality"
+                wire:model="nationality"
+                name="nationality"
+                type="text"
+                placeholder="{{ __('Nationality') }}"
+                class="mt-1 bg-lightGray" />
+            <x-input-error class="mt-2" :messages="$errors->get('nationality')" />
+        </div>
+        {{-- Birthdate --}}
+        <div class="mt-4">
+            <x-input-label for="birthdate" :value="__('Date of Birth')" :required="true"/>
+            <x-input-text 
+                id="birthdate"
+                wire:model="birthdate"
+                name="birthdate"
+                type="date"
+                class="mt-1 bg-lightGray" />
+            <x-input-error class="mt-2" :messages="$errors->get('birthdate')" />
+        </div>
+        {{-- Birthplace --}}
+        <div class="mt-4">
+            <x-input-label for="birthplace" :value="__('Place of Birth')" :required="true"/>
+            <x-input-text 
+                id="birthplace"
+                wire:model="birthplace"
+                name="birthplace"
+                type="text"
+                placeholder="{{ __('Place of Birth') }}"
+                class="mt-1 bg-lightGray" />
+            <x-input-error class="mt-2" :messages="$errors->get('birthplace')" />
+        </div>
+        {{-- Address --}}
+        <div class="mt-4">
+            <x-input-label for="address" :value="__('Address')" :required="true"/>
+            <x-input-text 
+                id="address"
+                wire:model="address"
+                name="address"
+                type="text"
+                placeholder="{{ __('Address') }}"
+                class="mt-1 bg-lightGray" />
+            <x-input-error class="mt-2" :messages="$errors->get('address')" />
+        </div>
+        {{-- Account Type --}}
+        <div class="mt-4 text-gray">
+            <x-input-label for="account_type" :value="__('Account Type')" :required="true" />
+            <x-input-select 
+                id="account_type" 
+                wire:model="account_type" 
+                name="account_type" 
+                class="mt-1 bg-lightGray">
+                <option selected hidden>Account Type</option>
+                <option value="Cabuyeño">Cabuyeño</option>
+                <option value="Non-Cabuyeño">Non-Cabuyeño</option>
+            </x-input-select>
+            <x-input-error class="mt-2" :messages="$errors->get('account_type')" />
+        </div>
+        {{-- Phone --}}
+        <div class="mt-4">
+            <x-input-label for="phone" :value="__('Phone Number')" :required="true"/>
+            <x-input-text 
+                id="phone"
+                wire:model="phone"
+                name="phone"
+                type="text"
+                class="mt-1 bg-lightGray ps-12 flex items-center" alignIcon="left">
+                <span class="text-bold text-darkGray">+63</span>
+            </x-input-text>
+            <x-input-error class="mt-2" :messages="$errors->get('phone')" />
+        </div>
+        {{-- Email --}}
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email Address')" :required="true"/>
+            <x-input-text 
+                id="email"
+                wire:model="email"
+                name="email"
+                type="email"
+                placeholder="{{ __('Email Address') }}"
+                class="mt-1 bg-lightGray"
+                />
+            <x-input-error class="mt-2" :messages="$errors->get('email')" />
+        </div>
+        {{-- Submit --}}
+        <div class="flex justify-end items-center mt-4 space-x-4">
+            {{-- Loading --}}
+            <div wire:loading>
+                Saving student...
+            </div>
+
+            @if($errors->any())
+                <div wire:loading.remove class="text-red">
+                    Failed to save.
+                </div>
+            @endif
+            {{-- x-on:click.prevent="$dispatch('close-modal')" --}}
+            <div class="flex space-x-4">
+                <x-button wire:click.prevent="cancel" btnType="secondary">Cancel</x-button>
+                <x-button type="submit" btnType="success">Save</x-button>
+            </div>
+        </div>
+    </form>
+</div>
--- resources/views/livewire/students.blade.php
+++ resources/views/livewire/students.blade.php
@@ -6,9 +6,9 @@
 	<div class="flex justify-between mt-4">
 		@include('livewire.includes.search')
 
-		<x-button x-on:click="$dispatch('open-modal', 'create-student')" btnType="success" class="flex space-x-2 items-center">
+		<x-button wire:click.prevent="create" btnType="success" class="flex space-x-2 items-center">
 			<i class="fa-solid fa-plus"></i>
-			<span>Add Student</span>
+			<span>Add New Student</span>
 		</x-button>
 	</div>
 	
@@ -30,226 +30,59 @@
 					<td class="px-6 py-4">{{ $student->first_name }}</td>
 					<td class="px-6 py-4">{{ $student->middle_name ?? "N/A" }}</td>
 					<td class="px-6 py-4 text-md flex space-x-4">
-						<button x-on:click="$dispatch('open-modal', 'view-student')" class="text-veryDarkGray hover:text-darkGray">
-							<i class="fa-solid fa-eye"></i>
-						</button>
-						<button x-on:click="$dispatch('open-modal', 'edit-student')" class="text-veryDarkGray hover:text-darkGray">
-							<i class="fa-solid fa-pen-to-square"></i>	
-						</button>
-						<button x-on:click="$dispatch('open-modal', 'delete-student')" class="text-red hover:text-lightRed">
-							<i class="fa-solid fa-trash"></i>
-						</button>
+						<x-button wire:click.prevent="show({{$student}})" btnType="success" textSize="text-xs">
+							View
+						</x-button>
+						<x-button wire:click.prevent="edit({{$student}})" btnType="warning" textSize="text-xs">
+							Edit
+						</x-button>
+						<x-button wire:click.prevent="delete({{$student}})" btnType="danger" textSize="text-xs">
+							Delete
+						</x-button>
 					</td>
 				</tr>
-			@endforeach			
-		@endslot
+			@endforeach
+		@endslot			
 	</x-table>
 
-	{{-- <div class="flex mt-4 items-center space-x-4">
-		<span class="text-sm text-darkGray">Per Page</span>
-		<x-input-select class="w-24">
-			<option selected value="10">10</option>
-			<option value="20">20</option>
-			<option value="50">50</option>
-			<option value="100">100</option>
-		</x-input-select>
-	</div> --}}
-
+	{{-- Pagination Links --}}
 	<div class="mt-4">
 		{{ $students->links() }}
 	</div>
 
 	{{-- Create Student Form --}}
-    <x-modal name="create-student" title="Add Student">
-		<form wire:submit="store">
-			{{-- Student Number --}}
+	<x-modal name="create-student" title="Add Student" focusable>
+		@include('livewire.includes.students-form')
+	</x-modal>
+
+	@if($selectedStudent)
+		{{-- View Student Form --}}
+		<x-modal name="show-student" title="Student Information" focusable>
 			<div>
-				<x-input-label for="student_no" :value="__('Student Number')" :required="true" />
-				<x-input-text 
-					id="student_no"
-					wire:model.live="student_no"
-					name="student_no"
-					type="text"
-					placeholder="{{ __('Student Number') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('student_no')" />
+				{{ $selectedStudent->first_name. ' ' .$selectedStudent->middle_name. ' ' .$selectedStudent->last_name }}
 			</div>
-			{{-- Last Name --}}
-			<div class="mt-4">
-				<x-input-label for="last_name" :value="__('Last Name')" :required="true" />
-				<x-input-text 
-					id="last_name"
-					wire:model.live="last_name"
-					name="last_name"
-					type="text"
-					placeholder="{{ __('Last Name') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('last_name')" />
-			</div>
-			{{-- First Name --}}
-			<div class="mt-4">
-				<x-input-label for="first_name" :value="__('First Name')" :required="true" />
-				<x-input-text 
-					id="first_name"
-					wire:model.live="first_name"
-					name="first_name"
-					type="text"
-					placeholder="{{ __('First Name') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('first_name')" />
-			</div>
-			{{-- Middle Name --}}
-			<div class="mt-4">
-				<x-input-label for="middle_name" :value="__('Middle Name')" />
-				<x-input-text 
-					id="middle_name"
-					wire:model.live="middle_name"
-					name="middle_name"
-					type="text"
-					placeholder="{{ __('Middle Name') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('middle_name')" />
-			</div>
-			{{-- Sex --}}
-			<div class="mt-4 text-gray">
-				<x-input-label for="sex" :value="__('Sex')" :required="true" />
-				<x-input-select 
-					id="sex" 
-					wire:model.live="sex" 
-					name="sex" 
-					class="mt-1 bg-lightGray">
-					<option selected hidden>Sex</option>
-					<option value="Male">Male</option>
-					<option value="Female">Female</option>
-				</x-input-select>
-				<x-input-error class="mt-2" :messages="$errors->get('sex')" />
-			</div>
-			{{-- Civil Status --}}
-			<div class="mt-4 text-gray">
-				<x-input-label for="civil_status" :value="__('Civil Status')" :required="true" />
-				<x-input-select 
-					id="civil_status" 
-					wire:model.live="civil_status" 
-					name="civil_status" 
-					class="mt-1 bg-lightGray">
-					<option selected hidden>Civil Status</option>
-					<option value="Single">Single</	option>
-					<option value="Married">Married</option>
-					<option value="Divorced">Divorced</option>
-					<option value="Widowed">Widowed</option>
-				</x-input-select>
-				<x-input-error class="mt-2" :messages="$errors->get('civil_status')" />
-			</div>
-			{{-- Nationality --}}
-			<div class="mt-4">
-				<x-input-label for="nationality" :value="__('Nationality')" :required="true"/>
-				<x-input-text 
-					id="nationality"
-					wire:model.live="nationality"
-					name="nationality"
-					type="text"
-					placeholder="{{ __('Nationality') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('nationality')" />
-			</div>
-			{{-- Birthdate --}}
-			<div class="mt-4">
-				<x-input-label for="birthdate" :value="__('Birthdate')" :required="true"/>
-				<x-input-text 
-					id="birthdate"
-					wire:model.live="birthdate"
-					name="birthdate"
-					type="date"
-					placeholder="{{ __('Birthdate') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('birthdate')" />
-			</div>
-			{{-- Birthplace --}}
-			<div class="mt-4">
-				<x-input-label for="birthplace" :value="__('Birthplace')" :required="true"/>
-				<x-input-text 
-					id="birthplace"
-					wire:model.live="birthplace"
-					name="birthplace"
-					type="text"
-					placeholder="{{ __('Birthplace') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('birthplace')" />
-			</div>
-			{{-- Address --}}
-			<div class="mt-4">
-				<x-input-label for="address" :value="__('Address')" :required="true"/>
-				<x-input-text 
-					id="address"
-					wire:model.live="address"
-					name="address"
-					type="text"
-					placeholder="{{ __('Address') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('address')" />
-			</div>
-			{{-- Account Type --}}
-			<div class="mt-4 text-gray">
-				<x-input-label for="account_type" :value="__('Account Type')" :required="true" />
-				<x-input-select 
-					id="account_type" 
-					wire:model.live="account_type" 
-					name="account_type" 
-					class="mt-1 bg-lightGray">
-					<option selected hidden>Account Type</option>
-					<option value="Cabuyeño">Cabuyeño</option>
-					<option value="Non-Cabuyeño">Non-Cabuyeño</option>
-				</x-input-select>
-				<x-input-error class="mt-2" :messages="$errors->get('account_type')" />
-			</div>
-			{{-- Phone --}}
-			<div class="mt-4">
-				<x-input-label for="phone" :value="__('Phone')" :required="true"/>
-				<x-input-text 
-					id="phone"
-					wire:model.live="phone"
-					name="phone"
-					type="text"
-					placeholder="{{ __('Phone') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('phone')" />
-			</div>
-			{{-- Email --}}
-			<div class="mt-4">
-				<x-input-label for="email" :value="__('Email Address')" :required="true"/>
-				<x-input-text 
-					id="email"
-					wire:model.live="email"
-					name="email"
-					type="email"
-					placeholder="{{ __('Email Address') }}"
-					class="mt-1 bg-lightGray" />
-				<x-input-error class="mt-2" :messages="$errors->get('email')" />
-			</div>
-			{{-- Submit --}}
-			<div class="flex justify-end mt-4 space-x-4">
-				<x-button x-on:click.prevent="$dispatch('close-modal')" btnType="secondary">Cancel</x-button>
-				<x-button type="submit" btnType="success">Add</x-button>
-			</div>
-			{{-- Loading --}}
-			{{-- <div wire:loading>
-				<i class="fa-solid fa-circle-info"></i>
-			</div> --}}
-		</form>
-	</x-modal>
+			
+			Student Number: {{ $selectedStudent->student_no }} <br>
+			Sex: {{ $selectedStudent->sex }} <br>
+			Civil Status: {{ $selectedStudent->civil_status }} <br>
+			Nationality: {{ $selectedStudent->nationality }} <br>
+			Date of Birth: {{ $selectedStudent->birthdate }} <br>
+			Place of Birth: {{ $selectedStudent->birthplace }} <br>
+			Address: {{ $selectedStudent->address }} <br>
+			Phone Number: {{ $selectedStudent->phone }} <br>
+			Email: {{ $selectedStudent->email }} <br>
+			Account Type: {{ $selectedStudent->account_type }}
+		</x-modal>
 
-	{{-- View Student Form --}}
-	<x-modal name="view-student" title="Student Information">
-		{{-- {{ $student->student_no }} --}}
-	</x-modal>
+		{{-- Edit Student Form --}}
+		<x-modal name="edit-student" title="Edit Student" focusable>
+			@include('livewire.includes.students-form')
+		</x-modal>
 
-	{{-- Edit Student Form --}}
-    <x-modal name="edit-student">
-	</x-modal>
-
-	{{-- Delete Student Dialog --}}
-    <x-modal name="delete-student">
-	</x-modal>
+		{{-- Delete Student Dialog --}}
+		<x-modal name="delete-student" title="Delete Student" focusable>
+		</x-modal>
+	@endif
 
 	@include('livewire.includes.toasts')
 </div>    

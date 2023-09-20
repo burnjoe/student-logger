29075b6ae92c18ffbbdf60147342a3506a9a4bf4
burnjoe <sabanajholo@gmail.com>
2023-09-20 17:10:27 +0800

    updated button + toaast style

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -9,14 +9,20 @@
 @php 
   switch ($btnType) {
     case 'secondary':
-      $bgClasses = 'bg-darkGray hover:bg-veryDarkGray';
+      $bgClasses = 'bg-darkGray hover:bg-gray active:bg-veryDarkGray focus:outline-none focus:ring focus:ring-gray';
       break;
     case 'success':
-      $bgClasses = 'bg-green hover:bg-lightGreen';
+      $bgClasses = 'bg-green hover:bg-lightGreen active:bg-darkGreen focus:outline-none focus:ring focus:ring-lightGreen';
+      break;
+    case 'warning':
+      $bgClasses = 'bg-orange hover:bg-lightOrange active:bg-darkOrange focus:outline-none focus:ring focus:ring-lightOrange';
+      break;
+    case 'danger':
+      $bgClasses = 'bg-red hover:bg-lightRed active:bg-darkRed focus:outline-none focus:ring focus:ring-lightRed';
       break;
     case 'primary':
     default:
-      $bgClasses = 'bg-blue hover:bg-lightBlue';
+      $bgClasses = 'bg-blue hover:bg-lightBlue active:bg-darkBlue focus:outline-none focus:ring focus:ring-lightBlue';
       break;
   }
 @endphp

38bb3ebee8ce6774caebb9e547ae869bc2c4d8af
burnjoe <sabanajholo@gmail.com>
2023-10-08 05:31:00 +0800

    added button disabled style

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -9,20 +9,20 @@
 @php 
   switch ($btnType) {
     case 'secondary':
-      $bgClasses = 'bg-darkGray hover:bg-gray active:bg-veryDarkGray focus:outline-none focus:ring focus:ring-gray';
+      $bgClasses = 'bg-darkGray disabled:bg-veryDarkGray hover:bg-gray active:bg-veryDarkGray focus:outline-none focus:ring focus:ring-gray';
       break;
     case 'success':
-      $bgClasses = 'bg-green hover:bg-lightGreen active:bg-darkGreen focus:outline-none focus:ring focus:ring-lightGreen';
+      $bgClasses = 'bg-green disabled:bg-darkGreen hover:bg-lightGreen active:bg-darkGreen focus:outline-none focus:ring focus:ring-lightGreen';
       break;
     case 'warning':
-      $bgClasses = 'bg-orange hover:bg-lightOrange active:bg-darkOrange focus:outline-none focus:ring focus:ring-lightOrange';
+      $bgClasses = 'bg-orange disabled:bg-darkOrange hover:bg-lightOrange active:bg-darkOrange focus:outline-none focus:ring focus:ring-lightOrange';
       break;
     case 'danger':
-      $bgClasses = 'bg-red hover:bg-lightRed active:bg-darkRed focus:outline-none focus:ring focus:ring-lightRed';
+      $bgClasses = 'bg-red disabled:bg-darkRed hover:bg-lightRed active:bg-darkRed focus:outline-none focus:ring focus:ring-lightRed';
       break;
     case 'primary':
     default:
-      $bgClasses = 'bg-blue hover:bg-lightBlue active:bg-darkBlue focus:outline-none focus:ring focus:ring-lightBlue';
+      $bgClasses = 'bg-blue disabled:bg-darkBlue hover:bg-lightBlue active:bg-darkBlue focus:outline-none focus:ring focus:ring-lightBlue';
       break;
   }
 @endphp

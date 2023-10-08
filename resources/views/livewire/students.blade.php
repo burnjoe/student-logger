ac7764c20db6d298cbfa2bdd1aaa7516258486fa
burnjoe <sabanajholo@gmail.com>
2023-10-08 10:25:28 +0800

    added permissions

--- resources/views/livewire/students.blade.php
+++ resources/views/livewire/students.blade.php
@@ -1,11 +1,22 @@
 <div>
+	<div class="grid grid-cols-2 gap-4">
+		<x-card>
+			Total Number Students
+		</x-card>
+		<x-card>
+			Pie Chart of Students per Dept.
+		</x-card>
+	</div>
+
 	<div class="flex justify-between mt-4">
 		@include('livewire.includes.search')
 
-		<x-button wire:click.prevent="create" btnType="success" class="flex space-x-2 items-center">
-			<i class="fa-solid fa-plus"></i>
-			<span>Add New Student</span>
-		</x-button>
+		@can('create students')
+			<x-button wire:click.prevent="create" btnType="success" class="flex space-x-2 items-center">
+				<i class="fa-solid fa-plus"></i>
+				<span>Add New Student</span>
+			</x-button>
+		@endcan
 	</div>
 	
 	{{-- Table --}}
@@ -26,15 +37,21 @@
 					<td class="px-6 py-4">{{ $student->first_name }}</td>
 					<td class="px-6 py-4">{{ empty($student->middle_name) ? "N/A" : $student->middle_name }}</td>
 					<td class="px-6 py-4 text-md flex space-x-4">
-						<x-button wire:click.prevent="show({{$student->id}})" btnType="success" textSize="text-xs">
-							View
-						</x-button>
-						<x-button wire:click.prevent="edit({{$student->id}})" btnType="warning" textSize="text-xs">
-							Edit
-						</x-button>
-						<x-button wire:click.prevent="delete({{$student->id}})" btnType="danger" textSize="text-xs">
-							Delete
-						</x-button>
+						@can('view students')
+							<x-button wire:click.prevent="show({{$student->id}})" btnType="success" textSize="text-xs">
+								View
+							</x-button>
+						@endcan
+						@can('edit students')
+							<x-button wire:click.prevent="edit({{$student->id}})" btnType="warning" textSize="text-xs">
+								Edit
+							</x-button>
+						@endcan
+						@can('delete students')
+							<x-button wire:click.prevent="delete({{$student->id}})" btnType="danger" textSize="text-xs">
+								Delete
+							</x-button>
+						@endcan
 					</td>
 				</tr>
 			@endforeach
@@ -47,36 +64,53 @@
 	</div>
 
 	{{-- Create Student Form --}}
-	<x-modal name="create-student" title="Add Student" focusable>
-		@include('livewire.includes.students-form')
-	</x-modal>
+	@can('create students')
+		<x-modal name="create-student" title="Add Student" focusable>
+			@include('livewire.includes.students-form')
+		</x-modal>
+	@endcan
 
 	{{-- View Student Form --}}
-	<x-modal name="show-student" title="Student Information" focusable>
-		<div>
-			{{ $first_name. ' ' .$middle_name. ' ' .$last_name }}
-		</div>
-		
-		Student Number: {{ $student_no }} <br>
-		Sex: {{ $sex }} <br>
-		Civil Status: {{ $civil_status }} <br>
-		Nationality: {{ $nationality }} <br>
-		Date of Birth: {{ $birthdate }} <br>
-		Place of Birth: {{ $birthplace }} <br>
-		Address: {{ $address }} <br>
-		Phone Number: {{ $phone }} <br>
-		Email: {{ $email }} <br>
-		Account Type: {{ $account_type }}
-	</x-modal>
+	@can('view students')
+		<x-modal name="show-student" title="Student Information" focusable>
+			<div>
+				{{ $first_name. ' ' .$middle_name. ' ' .$last_name }}
+			</div>
+			
+			Student Number: {{ $student_no }} <br>
+			Sex: {{ $sex }} <br>
+			Civil Status: {{ $civil_status }} <br>
+			Nationality: {{ $nationality }} <br>
+			Date of Birth: {{ $birthdate }} <br>
+			Place of Birth: {{ $birthplace }} <br>
+			Address: {{ $address }} <br>
+			Phone Number: {{ $phone }} <br>
+			Email: {{ $email }} <br>
+			Account Type: {{ $account_type }}
+
+			<div class="flex justify-end items-center mt-4">
+				<x-button 
+					x-on:click.prevent="$dispatch('close-modal')" 
+					btnType="secondary"
+					wire:loading.attr="disabled">
+					Close
+				</x-button>
+			</div>
+		</x-modal>
+	@endcan
 
 	{{-- Edit Student Form --}}
-	<x-modal name="edit-student" title="Edit Student" focusable>
-		@include('livewire.includes.students-form')
-	</x-modal>
+	@can('edit students')
+		<x-modal name="edit-student" title="Edit Student" focusable>
+			@include('livewire.includes.students-form')
+		</x-modal>
+	@endcan
 
 	{{-- Delete Student Dialog --}}
-	<x-modal name="delete-student" title="Delete Student" focusable>
-	</x-modal>
+	@can('delete students')
+		<x-modal name="delete-student" title="Delete Student" focusable>
+		</x-modal>
+	@endcan
 
 	@include('livewire.includes.toasts')
 </div>    

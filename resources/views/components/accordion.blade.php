ed0dfbf8571e2a7ca176bcd073112aad4ccfe82b
burnjoe <sabanajholo@gmail.com>
2023-10-10 18:42:09 +0800

    added accordion close event

--- resources/views/components/accordion.blade.php
+++ resources/views/components/accordion.blade.php
@@ -1,10 +1,7 @@
-@props([
-    'selected' => null,
-])
-
 <div x-data="() => ({
-        selected: @json($selected)
+        selected: null
     })" 
+    x-on:close-accordion.window="selected = null"
     {{ $attributes->merge(['class' => 'w-full mx-auto space-y-4']) }}>
     
     {{ $slot }}

0aa7fa94eb42c6bce25f43af044af9a9b829222b
burnjoe <sabanajholo@gmail.com>
2023-10-10 09:13:35 +0800

    added show + restore + delete permanently function

--- app/Livewire/StudentsArchive.php
+++ app/Livewire/StudentsArchive.php
@@ -2,15 +2,149 @@
 
 namespace App\Livewire;
 
+use App\Models\Student;
 use Livewire\Component;
 use Illuminate\Support\Facades\View;
+use Livewire\WithPagination;
 
 class StudentsArchive extends Component
 {
+    use WithPagination;
+
+    public $id;
+    public $student_no;
+    public $last_name;
+    public $first_name;
+    public $middle_name;
+    public $sex;
+    public $civil_status;
+    public $nationality;
+    public $birthdate;
+    public $birthplace;
+    public $address;
+    public $phone;
+    public $email;
+    public $account_type;
+    
+    public Student $selectedStudent;
+
+    public $action;
+    public $search = "";
+
+
     public function render()
     {
         View::share('page', 'archive');
 
-        return view('livewire.students-archive');
+        return view('livewire.students-archive', 
+        [
+            'students' => Student::onlyTrashed()
+                ->latest()
+                ->search($this->search)
+                ->paginate(15)
+        ]);
+    }
+
+    /**
+     *  Initialize attributes
+     */
+    public function init(int $id) 
+    {
+        try {
+            $this->selectedStudent = Student::onlyTrashed()->findOrFail($id);
+
+            $this->id = $this->selectedStudent->id;
+            $this->student_no = $this->selectedStudent->student_no;
+            $this->last_name = $this->selectedStudent->last_name;
+            $this->first_name = $this->selectedStudent->first_name;
+            $this->middle_name = $this->selectedStudent->middle_name;
+            $this->sex = $this->selectedStudent->sex;
+            $this->civil_status = $this->selectedStudent->civil_status;
+            $this->nationality = $this->selectedStudent->nationality;
+            $this->birthdate = $this->selectedStudent->birthdate;
+            $this->birthplace = $this->selectedStudent->birthplace;
+            $this->address = $this->selectedStudent->address;
+            $this->phone = $this->selectedStudent->phone;
+            $this->email = $this->selectedStudent->email;
+            $this->account_type = $this->selectedStudent->account_type;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+    
+    /**
+     * Show selected record in modal
+     */
+    public function show(int $id) 
+    {
+        $this->dispatch('close-modal');
+
+        try {
+            $this->init($id);
+
+            $this->dispatch('open-modal', 'show-student');
+        } catch (\Throwable $th) {
+            session()->flash('danger', 'Unable to view student');
+        }
+    }
+
+    /**
+     * Show restore confirmation dialog
+     */
+    public function restore($id) 
+    {
+        $this->dispatch('close-modal');
+
+        try {
+            $this->selectedStudent = Student::onlyTrashed()->findOrFail($id);
+            $this->action = "recover";
+            $this->dispatch('open-modal', 'restore-student');
+        } catch (\Throwable $th) {
+            session()->flash('danger', 'Unable to restore student');
+        }
+    }
+
+    /**
+     * Restore record
+     */
+    public function recover() 
+    {
+        $this->selectedStudent->restore();
+
+        $this->reset();
+
+        session()->flash('success', 'Student successfully deleted');
+
+        $this->dispatch('close-modal');
+    }
+
+    /**
+     * Show delete confirmation dialog
+     */
+    public function delete($id) 
+    {
+        $this->dispatch('close-modal');
+
+        try {
+            $this->selectedStudent = Student::onlyTrashed()->findOrFail($id);
+            $this->action = "destroy";
+            $this->dispatch('open-modal', 'delete-student');
+        } catch (\Throwable $th) {
+            session()->flash('danger', 'Unable to delete student permanently');
+        }
+    }
+
+    /**
+     * Permanently deletes record
+     */
+    public function destroy() 
+    {
+        $this->selectedStudent->forceDelete();
+
+        $this->reset();
+
+        session()->flash('success', 'Student successfully deleted');
+
+        $this->dispatch('close-modal');
     }
 }
--- resources/views/livewire/students-archive.blade.php
+++ resources/views/livewire/students-archive.blade.php
@@ -13,31 +13,79 @@
 		@endslot
 
         @slot('data')
-			{{-- @foreach ($students as $student)
+			@foreach ($students as $student)
 				<tr wire:key="{{ $student->id }}" class="text-sm border-b border-lightGray transition-all hover:bg-veryLightGreen">
 					<td class="px-6 py-4">{{ $student->id }}</td>
 					<td class="px-6 py-4">{{ $student->student_no }}</td>
 					<td class="px-6 py-4">{{ $student->last_name }}</td>
 					<td class="px-6 py-4">{{ $student->first_name }}</td>
 					<td class="px-6 py-4 text-md flex space-x-4">
-						@can('view students')
-							<x-button wire:click.prevent="show({{$student->id}})" btnType="success" textSize="text-xs">
-								View
-							</x-button>
-						@endcan
-						@can('edit students')
-							<x-button wire:click.prevent="edit({{$student->id}})" btnType="warning" textSize="text-xs">
-								Edit
-							</x-button>
-						@endcan
-						@can('delete students')
-							<x-button wire:click.prevent="delete({{$student->id}})" btnType="danger" textSize="text-xs">
-								Delete
-							</x-button>
-						@endcan
+						<x-button wire:click.prevent="show({{$student->id}})" btnType="success" textSize="text-xs">
+							View
+						</x-button>
+						<x-button wire:click.prevent="restore({{$student->id}})" btnType="secondary" textSize="text-xs">
+							Restore
+						</x-button>
+						<x-button wire:click.prevent="delete({{$student->id}})" btnType="danger" textSize="text-xs">
+							Delete Permanently
+						</x-button>
 					</td>
 				</tr>
-			@endforeach --}}
+			@endforeach
 		@endslot
     </x-table>
+
+	{{-- Pagination Links --}}
+	<div class="mt-4">
+		{{ $students->links() }}
+	</div>
+
+	{{-- View Student Form --}}
+	<x-modal wire:ignore.self name="show-student" title="Student Information" focusable>
+		<div>
+			{{ $first_name. ' ' .$middle_name. ' ' .$last_name }}
+		</div>
+		
+		Student Number: {{ $student_no }} <br>
+		Sex: {{ $sex }} <br>
+		Civil Status: {{ $civil_status }} <br>
+		Nationality: {{ $nationality }} <br>
+		Date of Birth: {{ $birthdate }} <br>
+		Place of Birth: {{ $birthplace }} <br>
+		Address: {{ $address }} <br>
+		Phone Number: {{ $phone }} <br>
+		Email: {{ $email }} <br>
+		Account Type: {{ $account_type }}
+
+		<div class="flex justify-end items-center mt-4">
+			<x-button 
+				x-on:click.prevent="$dispatch('close-modal')" 
+				btnType="secondary"
+				wire:loading.attr="disabled">
+				Close
+			</x-button>
+		</div>
+	</x-modal>
+
+	{{-- Restore Student Dialog --}}
+	<x-modal wire:ignore.self name="restore-student" title="Restore Student" maxWidth="lg" focusable>
+		@include('livewire.includes.confirm-form', [
+			'prompt' => 'Are you sure you want to restore this record?',
+			'btnType' => 'success',
+			'label' => 'Restore',
+			'labelLoading' => 'Restoring...'
+		])
+	</x-modal>
+
+	{{-- Delete Permanently Student Dialog --}}
+	<x-modal wire:ignore.self name="delete-student" title="Delete Student Permanently" maxWidth="lg" focusable>
+		@include('livewire.includes.confirm-form', [
+			'prompt' => 'Are you sure you want to delete this record permanently?',
+			'btnType' => 'danger',
+			'label' => 'Delete',
+			'labelLoading' => 'Deleting...'
+		])
+	</x-modal>
+
+	@include('livewire.includes.toasts')
 </div>

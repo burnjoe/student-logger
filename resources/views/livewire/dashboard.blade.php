e8f8371a14a0e02243c89a4291f8edee37b98977
burnjoe <sabanajholo@gmail.com>
2023-10-10 09:38:00 +0800

    updated StudentFactory + dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,5 +3,14 @@
         {{ 'Welcome, ' .Auth::user()->profileable->first_name. ' ' .Auth::user()->profileable->last_name. '!'}}
     </x-card>
 
+    <div class="grid grid-cols-2 gap-4 mt-4">
+		<x-card>
+			Total Number Students
+		</x-card>
+		<x-card>
+			Pie Chart of Students per Dept.
+		</x-card>
+	</div>
+
     @include('livewire.includes.toasts')
 </div>

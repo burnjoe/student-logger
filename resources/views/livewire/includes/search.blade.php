174eb48bf7601e6368e57e012226be32f1fc1b3d
burnjoe <sabanajholo@gmail.com>
2023-10-13 15:38:59 +0800

    lowered search debounce

--- resources/views/livewire/includes/search.blade.php
+++ resources/views/livewire/includes/search.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-input-text wire:model.live.debounce.500ms="search" type="text" alignIcon="left" placeholder="{{ $placeholder ?? 'Search' }}" class="max-w-sm">
+    <x-input-text wire:model.live.debounce.300ms="search" type="text" alignIcon="left" placeholder="{{ $placeholder ?? 'Search' }}" class="max-w-sm">
         <i class="fa-solid fa-magnifying-glass"></i>
     </x-input-text>
 </div>

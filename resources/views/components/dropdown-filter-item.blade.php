129b1186e9f98c884867febb60c7a5604a622741
cefeauin <127648869+cefeauin@users.noreply.github.com>
2023-11-03 19:34:06 +0800

    Revert "filtering ui design update:"
    This reverts commit 309cd3e25d479ab86541c3e8dcf0ca9cde6abca9.

--- resources/views/components/dropdown-filter-item.blade.php
+++
@@ -1,9 +0,0 @@
-@props([
-  'fontSize' => 'text-sm'
-])
-
-<a {{ $attributes->merge(['class' => 'flex block w-full px-4 py-2 text-left ' .$fontSize. ' rounded-lg transition-all hover:bg-lightGray']) }}>
-    <label>
-        {{ $slot }}
-    </label>
-</a>

dd5edbae56496f5a147e79281afcafb1bc43015b
burnjoe <sabanajholo@gmail.com>
2023-11-28 07:51:15 +0800

    implemented filters in table headers

--- resources/views/livewire/students-archive.blade.php
+++ resources/views/livewire/students-archive.blade.php
@@ -1,96 +1,78 @@
 <div>
-   <div class="grid lg:grid-cols-2 gap-4 mt-4">
-      @include('livewire.includes.search', ['placeholder' => 'Search by student no. or name'])
+    <div class="grid lg:grid-cols-2 gap-4 mt-4">
+        @include('livewire.includes.search', ['placeholder' => 'Search by student no. or name'])
+    </div>
 
-      {{-- Filtering --}}
-      <div class="flex justify-between lg:justify-end items-center">
-         <div class="flex justify-end items-center">
-            <div class="relative text-sm">
-               <i class="fa-solid fa-filter ps-2 text-darkGray"></i>
-               <span class="font-normal text-darkGray ps-1">Filter by:</span>
-            </div>
+    <x-table class="mt-4">
+        @slot('head')
+        <th class="px-6 py-4">Student No.</th>
+        <th class="px-6 py-4">Last Name</th>
+        <th class="px-6 py-4">First Name</th>
+        <th>@include('livewire.includes.filter-program')</th>
+        <th class="px-6 py-4">Action</th>
+        @endslot
 
-            {{-- Filter by college --}}
-            <div class="flex justify-start items-center ps-2">
-               @include('livewire.includes.filter-college')
-            </div>
+        @slot('data')
+        @foreach ($students as $student)
+        <tr wire:key="{{ $student->id }}"
+            class="text-sm border-b border-lightGray transition-all hover:bg-veryLightGreen">
+            <td class="px-6 py-4">{{ $student->student_no }}</td>
+            <td class="px-6 py-4">{{ $student->last_name }}</td>
+            <td class="px-6 py-4">{{ $student->first_name }}</td>
+            <td class="px-6 py-4">Program</td>
+            <td class="px-6 py-4 text-md flex space-x-4">
+                <x-button wire:click.prevent="show({{ $student->id }})" btnType="success" textSize="text-xs">
+                    View
+                </x-button>
+                <x-button wire:click.prevent="restore({{ $student->id }})" btnType="secondary" textSize="text-xs">
+                    Restore
+                </x-button>
+                <x-button wire:click.prevent="delete({{ $student->id }})" btnType="danger" textSize="text-xs">
+                    Delete Permanently
+                </x-button>
+            </td>
+        </tr>
+        @endforeach
+        @endslot
+    </x-table>
 
-            {{-- Filter by program --}}
-            <div class="flex justify-start items-center ps-2">
-               @include('livewire.includes.filter-program')
-            </div>
-         </div>
-      </div>
-   </div>
+    @if ($students->total() == 0)
+    <div class="flex justify-center py-6">
+        @if (empty($search))
+        No records found.
+        @else
+        No records found for matching "{{ $search }}".
+        @endif
+    </div>
+    @endif
 
-   <x-table class="mt-4">
-      @slot('head')
-         <th class="px-6 py-4">Student No.</th>
-         <th class="px-6 py-4">Last Name</th>
-         <th class="px-6 py-4">First Name</th>
-         <th class="px-6 py-4">Action</th>
-      @endslot
+    {{-- Pagination Links --}}
+    <div class="mt-4">
+        {{ $students->links() }}
+    </div>
 
-      @slot('data')
-         @foreach ($students as $student)
-            <tr wire:key="{{ $student->id }}"
-               class="text-sm border-b border-lightGray transition-all hover:bg-veryLightGreen">
-               <td class="px-6 py-4">{{ $student->student_no }}</td>
-               <td class="px-6 py-4">{{ $student->last_name }}</td>
-               <td class="px-6 py-4">{{ $student->first_name }}</td>
-               <td class="px-6 py-4 text-md flex space-x-4">
-                  <x-button wire:click.prevent="show({{ $student->id }})" btnType="success" textSize="text-xs">
-                     View
-                  </x-button>
-                  <x-button wire:click.prevent="restore({{ $student->id }})" btnType="secondary" textSize="text-xs">
-                     Restore
-                  </x-button>
-                  <x-button wire:click.prevent="delete({{ $student->id }})" btnType="danger" textSize="text-xs">
-                     Delete Permanently
-                  </x-button>
-               </td>
-            </tr>
-         @endforeach
-      @endslot
-   </x-table>
+    {{-- View Student Form --}}
+    <x-modal wire:ignore.self name="show-student" title="Student Information" focusable>
+        @include('livewire.includes.view-student-form')
+    </x-modal>
 
-   @if ($students->total() == 0)
-      <div class="flex justify-center py-6">
-         @if (empty($search))
-            No records found.
-         @else
-            No records found for matching "{{ $search }}".
-         @endif
-      </div>
-   @endif
+    {{-- Restore Student Dialog --}}
+    <x-modal wire:ignore.self name="restore-student" title="Restore Student" maxWidth="lg" focusable>
+        @include('livewire.includes.confirm-form', [
+        'prompt' => 'Are you sure you want to restore this record?',
+        'btnType' => 'success',
+        'label' => 'Restore',
+        'labelLoading' => 'Restoring...',
+        ])
+    </x-modal>
 
-   {{-- Pagination Links --}}
-   <div class="mt-4">
-      {{ $students->links() }}
-   </div>
-
-   {{-- View Student Form --}}
-   <x-modal wire:ignore.self name="show-student" title="Student Information" focusable>
-      @include('livewire.includes.view-student-form')
-   </x-modal>
-
-   {{-- Restore Student Dialog --}}
-   <x-modal wire:ignore.self name="restore-student" title="Restore Student" maxWidth="lg" focusable>
-      @include('livewire.includes.confirm-form', [
-          'prompt' => 'Are you sure you want to restore this record?',
-          'btnType' => 'success',
-          'label' => 'Restore',
-          'labelLoading' => 'Restoring...',
-      ])
-   </x-modal>
-
-   {{-- Delete Permanently Student Dialog --}}
-   <x-modal wire:ignore.self name="delete-student" title="Delete Student Permanently" maxWidth="lg" focusable>
-      @include('livewire.includes.confirm-form', [
-          'prompt' => 'Are you sure you want to delete this record permanently?',
-          'btnType' => 'danger',
-          'label' => 'Delete',
-          'labelLoading' => 'Deleting...',
-      ])
-   </x-modal>
+    {{-- Delete Permanently Student Dialog --}}
+    <x-modal wire:ignore.self name="delete-student" title="Delete Student Permanently" maxWidth="lg" focusable>
+        @include('livewire.includes.confirm-form', [
+        'prompt' => 'Are you sure you want to delete this record permanently?',
+        'btnType' => 'danger',
+        'label' => 'Delete',
+        'labelLoading' => 'Deleting...',
+        ])
+    </x-modal>
 </div>
--- resources/views/livewire/students.blade.php
+++ resources/views/livewire/students.blade.php
@@ -2,31 +2,14 @@
    <div class="grid lg:grid-cols-2 gap-4 mt-4">
       @include('livewire.includes.search', ['placeholder' => 'Search by student no. or name'])
 
-      {{-- Filtering --}}
       <div class="flex justify-between lg:justify-end items-center">
-         <div class="flex justify-start lg:justify-end items-center">
-            <div class="relative text-sm">
-               <i class="fa-solid fa-filter ps-2 text-darkGray"></i>
-               <span class="font-normal text-darkGray ps-1">Filter by:</span>
-            </div>
-
-            {{-- Filter by college --}}
-            <div class="flex justify-start items-center ps-2">
-               @include('livewire.includes.filter-college')
-            </div>
-
-            {{-- Filter by program --}}
-            <div class="flex justify-start items-center ps-2">
-               @include('livewire.includes.filter-program')
-            </div>
-         </div>
          @can('manage students')
-            <div class="flex justify-end ps-2">
-               <x-button wire:click.prevent="create" btnType="success" class="flex space-x-2 items-center">
-                  <i class="fa-solid fa-plus"></i>
-                  <span>Add New Student</span>
-               </x-button>
-            </div>
+         <div class="flex justify-end ps-2">
+            <x-button wire:click.prevent="create" btnType="success" class="flex space-x-2 items-center">
+               <i class="fa-solid fa-plus"></i>
+               <span>Add New Student</span>
+            </x-button>
+         </div>
          @endcan
       </div>
    </div>
@@ -34,45 +17,47 @@
    {{-- Table --}}
    <x-table class="mt-4">
       @slot('head')
-         <th class="px-6 py-4">Student No.</th>
-         <th class="px-6 py-4">Last Name</th>
-         <th class="px-6 py-4">First Name</th>
-         <th class="px-6 py-4">Action</th>
+      <th class="px-6 py-4">Student No.</th>
+      <th class="px-6 py-4">Last Name</th>
+      <th class="px-6 py-4">First Name</th>
+      <th>@include('livewire.includes.filter-program')</th>
+      <th class="px-6 py-4">Action</th>
       @endslot
 
       @slot('data')
-         @foreach ($students as $student)
-            <tr wire:key="{{ $student->id }}"
-               class="text-sm border-b border-lightGray transition-all hover:bg-veryLightGreen">
-               <td class="px-6 py-4">{{ $student->student_no }}</td>
-               <td class="px-6 py-4">{{ $student->last_name }}</td>
-               <td class="px-6 py-4">{{ $student->first_name }}</td>
-               <td class="px-6 py-4 text-md flex space-x-4">
-                  @can('manage students')
-                     <x-button wire:click.prevent="show({{ $student->id }})" btnType="success" textSize="text-xs">
-                        View
-                     </x-button>
-                     <x-button wire:click.prevent="edit({{ $student->id }})" btnType="warning" textSize="text-xs">
-                        Edit
-                     </x-button>
-                     <x-button wire:click.prevent="delete({{ $student->id }})" btnType="danger" textSize="text-xs">
-                        Delete
-                     </x-button>
-                  @endcan
-               </td>
-            </tr>
-         @endforeach
+      @foreach ($students as $student)
+      <tr wire:key="{{ $student->id }}"
+         class="text-sm border-b border-lightGray transition-all hover:bg-veryLightGreen">
+         <td class="px-6 py-4">{{ $student->student_no }}</td>
+         <td class="px-6 py-4">{{ $student->last_name }}</td>
+         <td class="px-6 py-4">{{ $student->first_name }}</td>
+         <td class="px-6 py-4">Program</td>
+         <td class="px-6 py-4 text-md flex space-x-4">
+            @can('manage students')
+            <x-button wire:click.prevent="show({{ $student->id }})" btnType="success" textSize="text-xs">
+               View
+            </x-button>
+            <x-button wire:click.prevent="edit({{ $student->id }})" btnType="warning" textSize="text-xs">
+               Edit
+            </x-button>
+            <x-button wire:click.prevent="delete({{ $student->id }})" btnType="danger" textSize="text-xs">
+               Delete
+            </x-button>
+            @endcan
+         </td>
+      </tr>
+      @endforeach
       @endslot
    </x-table>
 
    @if ($students->total() == 0)
-      <div class="flex justify-center py-6">
-         @if (empty($search))
-            No records found.
-         @else
-            No records found for matching "{{ $search }}".
-         @endif
-      </div>
+   <div class="flex justify-center py-6">
+      @if (empty($search))
+      No records found.
+      @else
+      No records found for matching "{{ $search }}".
+      @endif
+   </div>
    @endif
 
    {{-- Pagination Links --}}
@@ -82,28 +67,28 @@
 
    {{-- Create Student Form --}}
    @can('manage students')
-      <x-modal wire:ignore.self name="create-student" title="Add Student" focusable>
-         @include('livewire.includes.students-form')
-      </x-modal>
+   <x-modal wire:ignore.self name="create-student" title="Add Student" focusable>
+      @include('livewire.includes.students-form')
+   </x-modal>
 
-      {{-- View Student Form --}}
-      <x-modal wire:ignore.self name="show-student" title="Student Information" focusable>
-         @include('livewire.includes.view-student-form')
-      </x-modal>
+   {{-- View Student Form --}}
+   <x-modal wire:ignore.self name="show-student" title="Student Information" focusable>
+      @include('livewire.includes.view-student-form')
+   </x-modal>
 
-      {{-- Edit Student Form --}}
-      <x-modal wire:ignore.self name="edit-student" title="Edit Student" focusable>
-         @include('livewire.includes.students-form')
-      </x-modal>
+   {{-- Edit Student Form --}}
+   <x-modal wire:ignore.self name="edit-student" title="Edit Student" focusable>
+      @include('livewire.includes.students-form')
+   </x-modal>
 
-      {{-- Delete Student Dialog --}}
-      <x-modal wire:ignore.self name="delete-student" title="Delete Student" maxWidth="lg" focusable>
-         @include('livewire.includes.confirm-form', [
-             'prompt' => 'Are you sure you want to delete this record?',
-             'btnType' => 'danger',
-             'label' => 'Delete',
-             'labelLoading' => 'Deleting...',
-         ])
-      </x-modal>
+   {{-- Delete Student Dialog --}}
+   <x-modal wire:ignore.self name="delete-student" title="Delete Student" maxWidth="lg" focusable>
+      @include('livewire.includes.confirm-form', [
+      'prompt' => 'Are you sure you want to delete this record?',
+      'btnType' => 'danger',
+      'label' => 'Delete',
+      'labelLoading' => 'Deleting...',
+      ])
+   </x-modal>
    @endcan
 </div>

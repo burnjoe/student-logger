9bb603e5549dbdda407d262f2c24706f19b5cf68
burnjoe <sabanajholo@gmail.com>
2023-12-02 08:24:00 +0800

    added audit log view + filters

--- app/Livewire/AuditLog.php
+++ app/Livewire/AuditLog.php
@@ -2,15 +2,62 @@
 
 namespace App\Livewire;
 
+use App\Models\Employee;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\View;
+use Spatie\Activitylog\Models\Activity;
 
 class AuditLog extends Component
 {
+    use WithPagination;
+
+    public $search = "";
+    public $selectedEvents = [];
+    public $selectedRoles = [];
+
+    /**
+     * Renders the view
+     */
     public function render()
     {
         View::share('page', 'audit_log');
 
-        return view('livewire.audit-log');
+        return view('livewire.audit-log', [
+            'logs' => Activity::with([
+                'causer',
+                'causer.employee' =>
+                fn ($query) =>
+                $query->select('id', 'last_name', 'first_name'),
+            ])
+                ->when(
+                    $this->selectedEvents,
+                    fn ($query) =>
+                    $query->whereIn('event', $this->selectedEvents)
+                )
+                ->whereHas(
+                    'causer',
+                    fn ($query) =>
+                    $query->when(
+                        $this->selectedRoles,
+                        fn ($subquery) =>
+                        $subquery->roleIn($this->selectedRoles)
+                    )
+                )
+                ->whereHas(
+                    'causer.employee',
+                    fn ($query) =>
+                    $query->when(
+                        $this->search,
+                        fn ($subquery) =>
+                        $subquery->search($this->search)
+                    )
+                )
+                ->latest()
+                ->paginate(15),
+            'roles' => Role::select('id', 'name')
+                ->get(),
+        ]);
     }
 }
--- resources/views/livewire/audit-log.blade.php
+++ resources/views/livewire/audit-log.blade.php
@@ -1,3 +1,66 @@
+@php
+$eventColors = [
+    'created' => 'green',
+    'updated' => 'orange',
+    'deleted' => 'red',
+    'restored' => 'green',
+];
+@endphp
+
 <div>
-    {{-- Stop trying to control. --}}
+    {{-- Search --}}
+    <div class="grid grid-cols-2 gap-4 mt-4">
+        @include('livewire.includes.search', ['placeholder' => 'Search by user name'])
+    </div>
+
+    {{-- Table --}}
+    <x-table class="mt-4">
+        @slot('head')
+        <th class="ps-3">@include('livewire.includes.filter-event')</th>
+        <th class="px-6 py-4">Description</th>
+        <th class="px-6 py-4">User</th>
+        <th>@include('livewire.includes.filter-role')</th>
+        <th class="px-6 py-4">Timestamp</th>
+        <th class="px-6 py-4">Action</th>
+        @endslot
+
+        @slot('data')
+        @foreach ($logs as $log)
+        <tr wire:key="{{ $log->id }}" class="text-sm border-b border-lightGray transition-all hover:bg-veryLightGreen">
+            <td class="px-6 py-4">
+                <x-badge class="bg-{{$eventColors[$log->event]}} text-white" size="xs" fontWeight="semibold">
+                    {{ ucwords($log->event).' '.class_basename($log->subject_type) }}
+                </x-badge>
+            </td>
+            <td class="px-6 py-4">To Be Implemented Later</td>
+            <td class="px-6 py-4">{{ $log->causer->employee->first_name.' '.$log->causer->employee->last_name }}</td>
+            <td class="px-6 py-4">{{ ucwords($log->causer->roles->first()->name) }}</td>
+            <td class="px-6 py-4">{{ \Carbon\Carbon::parse($log->created_at)->format('Y-m-d h:i A') }}</td>
+            <td class="px-6 py-4">
+                @can('view audit log')
+                <x-button wire:click.prevent="" btnType="success" textSize="text-xs">
+					View
+				</x-button>
+                @endcan
+            </td>
+        </tr>
+        @endforeach
+        @endslot
+    </x-table>
+
+    @if($logs->total() == 0)
+    <div class="flex justify-center py-6">
+        @if(empty($search))
+        No records found.
+        @else
+        No records found for matching "{{$search}}".
+        @endif
+    </div>
+    @endif
+
+    {{-- Pagination Links --}}
+    <div class="mt-4">
+        {{ $logs->links() }}
+    </div>
+
 </div>

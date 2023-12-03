33dd6eee1c0515049d9926f7996a33e1e9e56994
burnjoe <sabanajholo@gmail.com>
2023-12-03 17:10:14 +0800

    added activity log for Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,10 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Employee extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, LogsActivity;
+
+    /**
+     * Events only recorded in activity log
+     */
+    protected static $recordEvents = [
+        'created',
+        'updated',
+        'deleted',
+        'restored',
+        'forceDeleted',
+    ];
 
     protected $fillable = [
         'last_name',
@@ -20,6 +33,38 @@ class Employee extends Model
         'address',
         'phone',
     ];
+
+
+    /**
+     * Activity logs option
+     */
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs()
+            ->useLogName('Employee')
+            ->setDescriptionForEvent(function (string $eventName) {
+                $attributes = $this->getDirty();
+                $old = $this->getAttributes();
+
+                switch ($eventName) {
+                    case 'created':
+                        return "Added New Employee: \"" . ($attributes['first_name'] . ' ' . $attributes['last_name']) . "\"";
+                    case 'updated':
+                        return "Updated Employee: \"" . ($old['first_name'] . ' ' . $old['last_name']) . "\"";
+                    case 'deleted':
+                        return "Archived Employee: \"" . ($old['first_name'] . ' ' . $old['last_name']) . "\"";
+                    case 'restored':
+                        return "Restored Employee: \"" . ($old['first_name'] . ' ' . $old['last_name']) . "\"";
+                    case 'forceDeleted':
+                        return "Deleted Employee Permanently: \"" . ($old['first_name'] . ' ' . $old['last_name']) . "\"";
+                    default:
+                        break;
+                }
+            });
+    }
 
 
     /**

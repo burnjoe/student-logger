72510a9cf2a9f0b25a79a2798da1f064982f7a78
burnjoe <sabanajholo@gmail.com>
2023-12-03 16:57:59 +0800

    added Student + Card activity logs

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -7,10 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Card extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, LogsActivity;
+
+    /**
+     * Events only recorded in activity log
+     */
+    protected static $recordEvents = [
+        'created',
+        'updated',
+        'deleted',
+        'restored',
+        'forceDeleted',
+    ];
 
     protected $fillable = [
         'rfid',
@@ -19,6 +32,37 @@ class Card extends Model
         'expires_at',
     ];
 
+
+    /**
+     * Activity logs option
+     */
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs()
+            ->useLogName('Card')
+            ->setDescriptionForEvent(function (string $eventName) {
+                $attributes = $this->getDirty();
+                $old = $this->getAttributes();
+
+                switch ($eventName) {
+                    case 'created':
+                        return "Added New Card: \"" . ($attributes['rfid'] . ' ' . $attributes['rfid']) . "\"";
+                    case 'updated':
+                        return "Updated Card: \"" . ($old['rfid'] . ' ' . $old['rfid']) . "\"";
+                    case 'deleted':
+                        return "Archived Card: \"" . ($old['rfid'] . ' ' . $old['rfid']) . "\"";
+                    case 'restored':
+                        return "Restored Card: \"" . ($old['rfid'] . ' ' . $old['rfid']) . "\"";
+                    case 'forceDeleted':
+                        return "Deleted Card Permanently: \"" . ($old['rfid'] . ' ' . $old['rfid']) . "\"";
+                    default:
+                        break;
+                }
+            });
+    }
 
     /**
      * Filtering search

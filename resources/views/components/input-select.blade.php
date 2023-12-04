d6ac558ef5a897025e804a15eb30ce9142cb5473
burnjoe <sabanajholo@gmail.com>
2023-12-05 00:57:00 +0800

    added bgColor to inputs + modal

--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,9 +1,12 @@
-@props(['messages'])
+@props([
+    'messages' => '',
+    'bgColor' => 'bg-white',
+])
 
 @php
 $errorStyle = $messages ? 'bg-veryLightRed border-red text-darkRed placeholder-darkRed' : 'text-veryDarkGray border-gray border-gray';
 @endphp
 
-<select {{ $attributes->merge(['class' => 'border text-sm rounded-md block w-full px-4 py-1.5 cursor-pointer '. $errorStyle]) }}>
+<select {{ $attributes->merge(['class' => 'border text-sm rounded-md block w-full px-4 py-1.5 cursor-pointer '. $bgColor .' '. $errorStyle]) }}>
     {{ $slot }}
 </select>

33e16ef383d4d12a28920248e7010ff40e6563ef
cefeauin <127648869+cefeauin@users.noreply.github.com>
2023-12-07 00:26:14 +0800

    role views in reports module

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,13 +1,13 @@
 <div>
    <x-card>
-      <span class="text-xl text-veryDarkGray font-semibold">
+      <span class="text-1rem font-semibold">
          {{ 'Welcome, ' . Auth::user()->employee->first_name . ' ' . Auth::user()->employee->last_name . '!' }}
       </span>
    </x-card>
 
    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
       <x-card>
-         <p class="text-xl text-veryDarkGray font-semibold">
+         <p class="text-1rem font-semibold">
             Total Number of Students:
          </p>
          <p class="text-6xl text-veryDarkGray font-bold">
@@ -18,7 +18,7 @@
       {{-- pie chart per department --}}
       <x-card>
          <div class="flex flex-col items-center">
-            <span class="text-xl text-veryDarkGray font-semibold">Number of Students per Dept.</span>
+            <span class="text-1rem font-semibold">Number of Students per Dept.</span>
             <canvas id="campusChart" style="max-width: 400px; max-height: 400px;"></canvas>
          </div>
       </x-card>
@@ -30,16 +30,14 @@
    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
    <script>
-      var ctx = document.getElementById('campusChart').getContext('2d');
-      var myChart = new Chart(ctx, {
+      new Chart(document.getElementById('campusChart').getContext('2d'), {
          type: 'pie',
          data: {
             labels: ['CAS', 'CBAA', 'CCS', 'COED', 'COE', 'CHAS'],
             datasets: [{
                label: ' # of Students',
                data: [10, 20, 30, 5, 9, 12, 9],
-               backgroundColor: ['rgb(153, 0, 0)', 'rgb(255, 205, 86)', 'rgb(255, 128, 0)', 'rgb(0, 102, 204)',
-                  'rgb(255, 102, 102)', 'rgb(0, 153, 0)'
+               backgroundColor: ['rgb(153, 0, 0)', 'rgb(255, 205, 86)', 'rgb(255, 128, 0)', 'rgb(0, 102, 204)', 'rgb(255, 102, 102)', 'rgb(0, 153, 0)'
                ],
                hoverOffset: 4
             }]

e86bfdbd53da5251756612f44d1436192a3151bf
burnjoe <sabanajholo@gmail.com>
2023-12-09 09:37:34 +0800

    added pnc_bg + modified input-error component

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-xs text-red space-y-1']) }}>
-        @foreach (array_unique((array) $messages) as $message)
-            <li>{{ $message }}</li>
-        @endforeach
-    </ul>
+<small {{ $attributes->merge(['class' => 'text-xs text-red']) }}>
+    {{ $messages[0] }}
+</small>
 @endif

f7d1acf72d59d9e0b929cce275dc9c724d3901fe
cefeauin <127648869+cefeauin@users.noreply.github.com>
2023-12-12 12:17:55 +0800

    dashboard module status chart data added from attendances

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -3,18 +3,27 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Attendance;
 use Illuminate\Support\Facades\View;
 
 class Dashboard extends Component
 {
-    /**
-     * Render livewire view
-     */
+    public $attendanceData = [];
+
+    public function mount()
+    {
+        $this->attendanceData = [
+            'IN' => Attendance::where('status', 'IN')->count(),
+            'OUT' => Attendance::where('status', 'OUT')->count(),
+            'MISSED' => Attendance::where('status', 'MISSED')->count(),
+        ];
+    }
+
     public function render()
     {
         session()->forget('auth.password_confirmed_at');
         View::share('page', 'dashboard');
         
-        return view('livewire.dashboard');
+        return view('livewire.dashboard', ['attendanceData' => $this->attendanceData,]);
     }
 }

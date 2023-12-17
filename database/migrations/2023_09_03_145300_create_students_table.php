17df5c9fa415e16794877eec2f25e9feb9163caf
burnjoe <sabanajholo@gmail.com>
2023-12-17 14:52:51 +0800

    revampled tables

--- database/migrations/2023_09_03_145300_create_students_table.php
+++ database/migrations/2023_09_03_145300_create_students_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('student_no')->unique();
+            $table->string('student_no')->unique();
             $table->string('last_name');
             $table->string('first_name');
             $table->string('middle_name')->nullable();
--- database/migrations/2023_09_13_220730_create_family_members_table.php
+++ database/migrations/2023_09_13_220730_create_family_members_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('last_name');
             $table->string('first_name');
-            $table->string('middle_name')->nullable();
             $table->enum('relationship', ['Father', 'Mother', 'Guardian']);
             $table->string('specified_relationship')->nullable();
             $table->string('occupation');

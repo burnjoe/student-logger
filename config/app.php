5729c875320d924d5fdb59c99d6e18b96d9b9afa
burnjoe <sabanajholo@gmail.com>
2023-12-18 09:23:35 +0800

    installed elgibor-solution query on encrypted

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        ESolution\DBEncryption\Providers\DBEncryptionServiceProvider::class,
     ])->toArray(),
 
     /*
